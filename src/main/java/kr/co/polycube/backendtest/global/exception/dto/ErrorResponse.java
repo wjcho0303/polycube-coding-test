4ab6e3cbea02bf1f631cd8cda64eb7ae9d9006fd
wjcho <wj930303@gmail.com>
2024-07-10 09:49:32 +0900

    Feat: ErrorResponse 기본 구조

--- src/main/java/kr/co/polycube/backendtest/global/exception/dto/ErrorResponse.java
+++ src/main/java/kr/co/polycube/backendtest/global/exception/dto/ErrorResponse.java
@@ -1,7 +1,25 @@
 package kr.co.polycube.backendtest.global.exception.dto;
 
+import kr.co.polycube.backendtest.global.exception.model.CustomException;
 import lombok.Getter;
 
 @Getter
 public class ErrorResponse {
+
+    private final String errorCode;
+    private final String message;
+    private final String detailMessage;
+
+    public ErrorResponse(CustomException ex) {
+        this.errorCode = ex.getErrorCode().getCode();
+        this.message = ex.getErrorCode().getMessage();
+        this.detailMessage = ex.getMessage();
+    }
+
+    public ErrorResponse(CustomException ex, String message) {
+        this.errorCode = ex.getErrorCode().getCode();
+        this.message = ex.getErrorCode().getMessage();
+        this.detailMessage = message;
+    }
+
 }
