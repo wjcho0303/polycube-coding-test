9b86987940c218f7d09e0410f135f0e8d84fb63a
wjcho <wj930303@gmail.com>
2024-07-10 09:41:02 +0900

    Feat: 예외 처리 소스파일 기본 생성

---
+++ src/main/java/kr/co/polycube/backendtest/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,12 @@
+package kr.co.polycube.backendtest.global.exception;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RequiredArgsConstructor
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
---
+++ src/main/java/kr/co/polycube/backendtest/global/exception/dto/ErrorResponse.java
@@ -0,0 +1,7 @@
+package kr.co.polycube.backendtest.global.exception.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+}
